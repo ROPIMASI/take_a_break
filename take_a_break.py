5b37d1661f6a35165916dafb66d769d817710ba9
ROPIMASI <ropimasi@email.com>
2019-05-08 21:03:45 -0300

    Unlicensed 1.

--- take_a_break.py
+++ take_a_break.py
@@ -10,10 +10,30 @@
 # PLATAFORM: Microsoft Windows7, PYTHON 3.7 INTERPRETER, VISUAL STUDIO CODE.
 
 # IMPORTANT NOTE / DISCLAIMER:
-# This is a project made exclusively for study purposes. Nobody has rights,
-# obligations, either responsibility on it or its effects. There is no gua-
-# rantee it will work correctly. It is not proprietary software, has no price,
-# can not be sold.
+# This is a project made exclusively for study purposes. Nobody has obliga-
+# tions, either responsibility on it or its effects. There is no guarantee
+# it will work correctly.
+
+# This is free and unencumbered software released into the public domain.
+# Anyone is free to copy, modify, publish, use, compile, sell, or
+# distribute this software, either in source code form or as a compiled
+# binary, for any purpose, commercial or non-commercial, and by any
+# means.
+# In jurisdictions that recognize copyright laws, the author or authors
+# of this software dedicate any and all copyright interest in the
+# software to the public domain. We make this dedication for the benefit
+# of the public at large and to the detriment of our heirs and
+# successors. We intend this dedication to be an overt act of
+# relinquishment in perpetuity of all present and future rights to this
+# software under copyright law.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+# For more information, please refer to <http://unlicense.org>
 """
 
 
